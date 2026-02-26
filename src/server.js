6132ede93e03b6c02cb468e02c0dcf381e11d94c
MUCYO BERTRAND <mucyobertrand3@gmail.com>
2026-02-26 09:09:45 +0200

    Recipe Finder Backend

---
+++ src/server.js
@@ -0,0 +1,64 @@
+import express from 'express'
+import { ENV } from "./config/env.js"
+import { db } from './config/db.js';
+import { favoritesTable } from './db/schema.js';
+import { and, eq } from "drizzle-orm";
+
+const app = express()
+const PORT=ENV.PORT;
+
+app.use(express.json())
+
+app.get("/api/health", (req,res)=>{
+    return res.status(200).json({success:true})
+})
+
+app.post("/api/favorites", async (req, res) => {
+    const { userId,recipeId , title , image , cookTime , servings } = req.body;
+    if(!userId || !recipeId || !title){
+        return res.status(400).json({error:"Missing required fields"})
+    }
+
+    try {
+        const newFavorites = await db.insert(favoritesTable).values({
+            userId,
+            recipeId,
+            title,
+            image,
+            cookTime,
+            servings
+        }).returning();
+        return res.status(201).json(newFavorites);
+    } catch (error) {
+        return res.status(500).json({error:"Failed to add favorite"})
+    }
+})
+
+app.delete("/api/favorites/:userId/:recipeId", async(req,res)=>{
+    try {
+        const { userId, recipeId } = req.params;
+        if(!userId || !recipeId){
+            return res.status(400).json({error:"Missing required fields"})
+        }
+        
+        await db.delete(favoritesTable).where(
+            and(eq(favoritesTable.userId, userId), eq(favoritesTable.recipeId, Number(recipeId)))
+        ).returning();
+        
+        return res.status(200).json("Favorites removed successfully");
+    } catch (error) {
+        console.log("Error in removing favorites: ",error);
+        return res.status(500).json({error:"Failed to remove favorite"})
+    }
+})
+
+app.get("/api/favorites/:userId", async (req,res)=>{
+    const { userId} = req.params;
+
+    const userFavorites = await db.select().from(favoritesTable).where(eq(favoritesTable.userId, userId));
+    return res.status(200).json(userFavorites);
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`)
+})
